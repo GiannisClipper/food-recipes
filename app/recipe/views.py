a9bd6b6d0a529c43c7a7bbe1bce59bc93bdabcf5
giannisclipper <strunstrun@gmail.com>
2019-08-30 16:50:29 +0300

    recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,3 +54,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Retrive the recipes for the authenticated user"""
 
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class (recipe-list present tags
+        and ingredients as id's while recipe-detail present full data)"""
+
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
