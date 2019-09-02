48ee22d8d96173bcf0f5817d2428a0f35cb773de
giannisclipper <strunstrun@gmail.com>
2019-09-02 13:41:36 +0300

    upload files

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,7 @@ import os
 
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image"""
+
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
